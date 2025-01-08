3fc544e3330c1ab33f33fccf2990e2f90ac022f8
ITTNikita <nikita.agarwal@intimetec.com>
2025-01-08 11:56:33 +0530

    from country code to adjacency country

--- Assignment1/CountryCode.ts
+++ Assignment1/CountryCode.ts
@@ -1,32 +1,30 @@
-import countryCodeData from './CountryData.json';
-interface CountryCodeData {
-    [key: string]: string;
+import adjacencyData from './CountryData.json';
+interface CountryAdjacencyData {
+  [key: string]: string[];
 }
-const countryData: CountryCodeData = countryCodeData as CountryCodeData;
+const countryAdjacency: CountryAdjacencyData = adjacencyData as CountryAdjacencyData;
+function getAdjacentCountryListByCountryCode(countryCode: string): string[] | string {
+  if (countryAdjacency[countryCode]) {
+    return countryAdjacency[countryCode];
+  }
+  return "Invalid country code or no adjacent countries found.";
+}
 
-let readline=require("readline");
-let read= readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
+const readline = require("readline");
+const read = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-read.question("Enter country code (e.g., IN, US, CA): ", (countryCode: string) => {      
-    let countryName = getCountryName(countryCode.toUpperCase());
-    console.log(countryName);
-    read.close();
-  });
-
-function getCountryName(countryCode:string):string{
-     if(countryCode in countryData)
-     {
-        return countryData[countryCode]
-     }
-     else{
-         return "Please enter the valid country code";
-     }  
- };
-
-
-
-
-
+read.question("Enter a country code (e.g., IN, US, NZ): ", (countryCode: string) => {
+  countryCode = countryCode.trim().toUpperCase();
+  const result = getAdjacentCountryListByCountryCode(countryCode);
+  
+  if (Array.isArray(result)) {
+    console.log(`Adjacent countries for ${countryCode}:`);
+    result.forEach((country) => console.log(`${country}`));
+  } else {
+    console.log(result);
+  }  
+  read.close();
+});
